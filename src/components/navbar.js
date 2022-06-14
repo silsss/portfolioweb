b3801a0e7cbc974fbfd3894d225be555d81853ee
silsss <silja-s@hotmail.com>
2022-06-14 19:25:12 +0100

    reorder added to navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
+import ReorderIcon from '@mui/icons-material/Reorder';
 import "../styles/Navbar.css";
 
 function Navbar() {
@@ -19,6 +20,7 @@ function Navbar() {
             setExpandNavbar((prev) => !prev);
           }}
         >
+          <ReorderIcon />
         </button>
       </div>
       <div className="links">
